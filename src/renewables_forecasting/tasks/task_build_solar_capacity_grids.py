2b6df2d09e206e9bbf44ac2cfa2b48e4e2361972
ilkajw <s6ilwull@uni-bonn.de>
2026-03-22 21:40:03 +0100

    Separate tasks for capacity grid building for solar and wind

---
+++ src/renewables_forecasting/tasks/task_build_solar_capacity_grids.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+from renewables_forecasting.config.paths import (
+    MASTR_SOLAR_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+    GRID_REFERENCE_DS_STORE,
+    SOLAR_CAPACITY_GRIDS_ZARR_STORE
+)
+from renewables_forecasting.data.capacity_grids import build_capacity_grids
+
+
+# ── Build daily grids of solar power capacity over Germany from coordinate-located plants,
+# their effective active periods and capacity generation data ─────────────────────────────
+
+def task_build_solar_capacity_grids(
+        depends_on=MASTR_SOLAR_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+        produces=SOLAR_CAPACITY_GRIDS_ZARR_STORE / ".complete"
+):
+
+    build_capacity_grids(
+        ref_weather_ds=GRID_REFERENCE_DS_STORE,
+        plants_csv=MASTR_SOLAR_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+        start_month=date(2015, 1, 1),
+        end_month=date(2025, 12, 1),
+        out_dir=SOLAR_CAPACITY_GRIDS_ZARR_STORE,
+    )
+
+    (SOLAR_CAPACITY_GRIDS_ZARR_STORE / ".complete").touch()
---
+++ src/renewables_forecasting/tasks/task_build_wind_capacity_grids.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+from renewables_forecasting.config.paths import (
+    MASTR_WIND_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+    GRID_REFERENCE_DS_STORE,
+    WIND_CAPACITY_GRIDS_ZARR_STORE
+)
+from renewables_forecasting.data.capacity_grids import build_capacity_grids
+
+
+# ── Build daily grids of wind power capacity over Germany from coordinate-located plants,
+# their effective active periods and capacity generation data ─────────────────────────────
+
+def task_build_wind_capacity_grids(
+        depends_on=MASTR_WIND_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+        produces=WIND_CAPACITY_GRIDS_ZARR_STORE / ".complete"
+):
+
+    build_capacity_grids(
+        ref_weather_ds=GRID_REFERENCE_DS_STORE,
+        plants_csv=MASTR_WIND_PLANTS_EFFECTIVE_START_WITH_COORDS_CSV,
+        start_month=date(2015, 1, 1),
+        end_month=date(2025, 12, 1),
+        out_dir=WIND_CAPACITY_GRIDS_ZARR_STORE,
+    )
+
+    (WIND_CAPACITY_GRIDS_ZARR_STORE / ".complete").touch()
