cce469bdafba256b504e44a270f975e2ac1b97c1
ilkajw <s6ilwull@uni-bonn.de>
2026-03-27 19:06:22 +0100

    delete cet conversion step from pytask era5 solar pipeline. correct input/output paths for utc-only pipeline

--- src/renewables_forecasting/tasks/task_era5_solar_download_preproc.py
+++ src/renewables_forecasting/tasks/task_era5_solar_download_preproc.py
@@ -4,12 +4,10 @@ from renewables_forecasting.config.data_sources import ERA5_SOLAR_VARIABLES
 from renewables_forecasting.config.paths import (
     ERA5_RAW_SOLAR_DATA_DIR,
     DAYLIGHT_MASK_NETCDF_PATH,
-    ERA5_CET_SOLAR_DATA_DIR,
     ERA5_MASKED_SOLAR_DATA_DIR
 )
 from renewables_forecasting.data.era5 import (
     download_era5,
-    convert_era5_utc_to_german_time,
     build_daylight_mask,
     apply_daylight_mask_to_era5_variables
 )
@@ -30,30 +28,14 @@ def task_download_era5_solar(produces=ERA5_RAW_SOLAR_DATA_DIR / ".complete"):
     (ERA5_RAW_SOLAR_DATA_DIR / ".complete").touch()
 
 
-# ── Convert time dimension from UTC to CET ────────────────────────────────────────────────────────
-
-def task_convert_era5_solar_data_to_cet(
-        depends_on=ERA5_RAW_SOLAR_DATA_DIR / ".complete",
-        produces=ERA5_CET_SOLAR_DATA_DIR / ".complete"
-):
-    convert_era5_utc_to_german_time(
-        in_dir=ERA5_RAW_SOLAR_DATA_DIR,
-        out_dir=ERA5_CET_SOLAR_DATA_DIR,
-        variables=list(ERA5_SOLAR_VARIABLES.keys()),
-        file_pattern="{variable}_{year}-{month:02d}.nc",
-    )
-
-    (ERA5_CET_SOLAR_DATA_DIR / ".complete").touch()
-
-
 # ── Construct a mask of daylight hours + buffer hours ───────────────────────────────────────
 
 def task_build_daylight_mask(
-        depends_on=ERA5_CET_SOLAR_DATA_DIR / ".complete",
+        depends_on=ERA5_RAW_SOLAR_DATA_DIR / ".complete",
         produces=DAYLIGHT_MASK_NETCDF_PATH
 ):
     build_daylight_mask(
-        ssrd_dir=ERA5_CET_SOLAR_DATA_DIR / "ssrd",
+        ssrd_dir=ERA5_RAW_SOLAR_DATA_DIR / "ssrd",
         out_path=DAYLIGHT_MASK_NETCDF_PATH,
         buffer_hours=2,
         file_pattern="ssrd_{year}-{month:02d}.nc"
@@ -65,7 +47,7 @@ def task_build_daylight_mask(
 def task_apply_daylight_mask_to_solar_data(
         depends_on={
             "mask": DAYLIGHT_MASK_NETCDF_PATH,
-            "cet_era5": ERA5_CET_SOLAR_DATA_DIR / ".complete"
+            "era5": ERA5_RAW_SOLAR_DATA_DIR / ".complete"
         },
         produces=ERA5_MASKED_SOLAR_DATA_DIR / ".complete"
 ):
@@ -73,7 +55,7 @@ def task_apply_daylight_mask_to_solar_data(
     apply_daylight_mask_to_era5_variables(
         mask_path=DAYLIGHT_MASK_NETCDF_PATH,
         variables=list(ERA5_SOLAR_VARIABLES.keys()),
-        era5_dir=ERA5_CET_SOLAR_DATA_DIR,
+        era5_dir=ERA5_RAW_SOLAR_DATA_DIR,
         out_dir=ERA5_MASKED_SOLAR_DATA_DIR,
     )
 
