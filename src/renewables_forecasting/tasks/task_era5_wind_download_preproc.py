46a235a2dd01034a2fd64e2c2dd26d5cac7ce0cb
ilkajw <s6ilwull@uni-bonn.de>
2026-03-27 11:40:03 +0100

    add pytask pipeline to download era5 wind variables within wind-specific bounds plus calculate wind speeds from directional wind components

---
+++ src/renewables_forecasting/tasks/task_era5_wind_download_preproc.py
@@ -0,0 +1,48 @@
+from datetime import date
+
+from renewables_forecasting.config.data_sources import ERA5_WIND_VARIABLES
+from renewables_forecasting.config.data_constants import WIND_LAT_MAX
+from renewables_forecasting.config.paths import (
+    ERA5_RAW_WIND_DATA_DIR,
+    ERA5_PROCESSED_WIND_DATA_UTC_DIR,
+    ERA5_PROCESSED_WIND_DATA_CET_DIR
+)
+from renewables_forecasting.data.era5 import (
+    download_era5,
+    calculate_wind_speed_from_components,
+    convert_era5_utc_to_german_time
+)
+
+
+# ── Download era5 u100, v100 wind components ───────────────────────────────────────────────────────────────
+
+def task_download_era5_wind(
+        produces=ERA5_RAW_WIND_DATA_DIR / ".complete"
+):
+
+    # Coordinates bounding box defaults to Germany
+    download_era5(
+        variables=ERA5_WIND_VARIABLES,
+        start=date(2015, 1, 1),
+        end=date(2025, 12, 1),  # Defaulting to months. Includes full december
+        store_dir=ERA5_RAW_WIND_DATA_DIR,
+        lat_max=WIND_LAT_MAX  # 0.25 degrees more derived from north-most offshore wind park
+    )
+
+    (ERA5_RAW_WIND_DATA_DIR / ".complete").touch()
+
+
+# ── Calculate wind speeds from wind components per grid cell ─────────────────────────────────────────────────────────
+
+def task_calculate_wind_speed(
+        depends_on=ERA5_RAW_WIND_DATA_DIR / ".complete",
+        produces=ERA5_PROCESSED_WIND_DATA_UTC_DIR / ".complete"
+):
+    calculate_wind_speed_from_components(
+        in_dir=ERA5_RAW_WIND_DATA_DIR,
+        out_dir=ERA5_PROCESSED_WIND_DATA_UTC_DIR,
+        file_pattern="{variable}_{year}-{month:02d}.nc",
+    )
+
+    (ERA5_PROCESSED_WIND_DATA_UTC_DIR / ".complete").touch()
+
